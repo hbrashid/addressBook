f02813ceafb67a79e8170057d0a0564abf7a4ebe
hbrashid <hbrashid@gmail.com>
2020-02-21 11:25:13 -0600

    Address book complete, with buttons

--- main.js
+++ main.js
@@ -1,32 +1,89 @@
 
-window.onload = function() {
-    oneUser()
-}
+// window.onload = function() {
+//     oneUser()
+// }
   
 
-const oneUser = () => {
-fetch('https://randomuser.me/api/')
-.then((response) => {
-    return response.json();
-})
-.then((response) => {
-    getPic(response);
-})
+function oneUser(i) {
+    fetch('https://randomuser.me/api/')
+  .then((response) => {
+      return response.json();
+  })
+  .then((response) => {
+      getPic(response, i);
+  })
+    .catch(error => {
+    console.error(error);
+  })
+  }
+  
+  
+  function getMoreData(){
+    for (let i = 1; i < 5; i++) {
+      const elem = document.getElementById('moreData' + i);
+      if(elem){
+        elem.addEventListener('click', () => {
+          document.getElementById('moreData' + i).previousSibling.style.display = "block"
+          });
+      }
+  }
+  }
+  
+  function getPic(response, i){
+  
+    const picText = `<div> <img src="${response.results[0].picture.large}"/> `;
+    
+    const info = `${response.results[0].name.first}, ${response.results[0].name.last}  `;
+    
+    document.getElementById('picDiv').innerHTML += picText + info;
+    
+    const moreData = `<div id="moreData" style="display:none"> ${response.results[0].dob.date}, ${response.results[0].location.street.name}, ${response.results[0].location.street.number}</div>`;
+    
+    document.getElementById('picDiv').innerHTML += moreData;
+    
+    const button = `<button id=moreData${i}>More details</button> <br /><br />`;
+    
+    document.getElementById('picDiv').innerHTML += button;
+  
+    
+    getMoreData();
+   
+  }
+  
+  
+  
+  for (let i = 1; i < 5; i++){
+      oneUser(i);
+  }
+  
 
-}
 
-for (let i = 1; i < 5; i++){
-    oneUser();
-}
 
-const getPic = (response) => {
-    // console.log(response.results.gender);
-    let picText = `<div> <img src="${response.results[0].picture.large}"/> `;
-    let info = `${response.results[0].name.first}, ${response.results[0].name.last}  `;
-    let moreData = `${response.results[0].dob.date}, ${response.results[0].location.street.name}, ${response.results[0].location.street.number}</div>`;
-    document.getElementById('picDiv').innerHTML += picText + info + moreData;
+
+
+
+// const oneUser = () => {
+// fetch('https://randomuser.me/api/')
+// .then((response) => {
+//     return response.json();
+// })
+// .then((response) => {
+//     getPic(response);
+// })
+
+// }
+
+// for (let i = 1; i < 5; i++){
+//     oneUser();
+// }
+
+// const getPic = (response) => {
+//     // console.log(response.results.gender);
+//     let picText = `<div> <img src="${response.results[0].picture.large}"/> `;
+//     let info = `${response.results[0].name.first}, ${response.results[0].name.last}  `;
+//     let moreData = `${response.results[0].dob.date}, ${response.results[0].location.street.name}, ${response.results[0].location.street.number}</div>`;
+//     document.getElementById('picDiv').innerHTML += picText + info + moreData;
     
 
 
-}
-    
+// }
