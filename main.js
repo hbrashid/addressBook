9bcee92f26225d744f69d89a9a75eac17cd4ebaa
hbrashid <hbrashid@gmail.com>
2020-02-21 00:20:02 -0600

    more testing

--- main.js
+++ main.js
@@ -21,8 +21,12 @@ for (let i = 1; i < 5; i++){
 
 const getPic = (response) => {
     // console.log(response.results.gender);
-    let picText = `<div> <img src="${response.results[0].picture.large}"/>`;
-    let info = `${response.results[0].name.first}, ${response.results[0].name.last}  </div>`
-    document.getElementById('picDiv').innerHTML += picText + info;
+    let picText = `<div> <img src="${response.results[0].picture.large}"/> `;
+    let info = `${response.results[0].name.first}, ${response.results[0].name.last}  `;
+    let moreData = `${response.results[0].dob.date}, ${response.results[0].location.street.name}, ${response.results[0].location.street.number}</div>`;
+    document.getElementById('picDiv').innerHTML += picText + info + moreData;
     
+
+
 }
+    
