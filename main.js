f01a4927350446051d01885007dd8fa4cadb28ff
hbrashid <hbrashid@gmail.com>
2020-02-26 19:39:07 -0600

    Added test stuff

--- main.js
+++ main.js
@@ -3,6 +3,8 @@
 //     oneUser()
 // }
   
+const assert = require('assert');
+
 
 function oneUser(i) {
     fetch('https://randomuser.me/api/')
@@ -57,33 +59,15 @@ function oneUser(i) {
   }
   
 
+  // Tests
 
+  if (typeof describe === 'function'){
 
+    describe('newPlayer', () => {
+      it('should make a player from the person list', () => {
+          let newPlayer = new Player(true, 'newPlayer.name');
+            assert.equal(newPlayer.id, true);
+        });
+    });
 
-
-
-// const oneUser = () => {
-// fetch('https://randomuser.me/api/')
-// .then((response) => {
-//     return response.json();
-// })
-// .then((response) => {
-//     getPic(response);
-// })
-
-// }
-
-// for (let i = 1; i < 5; i++){
-//     oneUser();
-// }
-
-// const getPic = (response) => {
-//     // console.log(response.results.gender);
-//     let picText = `<div> <img src="${response.results[0].picture.large}"/> `;
-//     let info = `${response.results[0].name.first}, ${response.results[0].name.last}  `;
-//     let moreData = `${response.results[0].dob.date}, ${response.results[0].location.street.name}, ${response.results[0].location.street.number}</div>`;
-//     document.getElementById('picDiv').innerHTML += picText + info + moreData;
-    
-
-
-// }
+  }
